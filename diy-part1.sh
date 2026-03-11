1053052e997c596fb8cde5dd459580a79b6a1c71
sunday9909 <59503145+sunday9909@users.noreply.github.com>
2026-03-11 22:56:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'src-git nikki https://github.com' >> feeds.conf.default
-echo 'src-git taskplan https://github.com' >> feeds.conf.default
-echo 'src-git parentcontrol https://github.com' >> feeds.conf.default
-echo 'src-git edge_theme https://github.com' >> feeds.conf.default
-echo 'src-git airconnect https://github.com' >> feeds.conf.default
-echo 'src-git airplay2 https://github.com' >> feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >> feeds.conf.default
+echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan' >> feeds.conf.default
+echo 'src-git parentcontrol https://github.com/sirpdboy/luci-app-parentcontrol.git' >> feeds.conf.default
+echo 'src-git edge_theme https://github.com/davinyue/luci-theme-edge.git' >> feeds.conf.default
+echo 'src-git airconnect https://github.com/sbwml/luci-app-airconnect.git' >> feeds.conf.default
+echo 'src-git airplay2 https://github.com/sbwml/luci-app-airplay2.git' >> feeds.conf.default
 
